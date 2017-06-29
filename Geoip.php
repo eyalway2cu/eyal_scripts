0dbca135baa188521f972ee0f0388dadffc4cf5d
Eyal Gershon <eyal@way2cu.com>
2017-06-29 15:19:42 +0300

    Update Geoip.php

--- Geoip.php
+++ Geoip.php
@@ -3,4 +3,7 @@ $userip = $_SERVER['REMOTE_ADDR'];
 $data = file_get_contents('http://freegeoip.net/json/' . $userip);
 $city = json_decode($data)->city);
 $state = json_decode($data)->region_name);
+if (!isset($city)){
+  $city = "In Your Area";
+}
 ?>
