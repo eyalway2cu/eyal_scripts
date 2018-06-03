af5c5e6f1c4a7a6af371c782baa888c3609f72cd
Eyal Gershon <eyal@way2cu.com>
2018-06-03 10:59:17 +0300

    Update form_webhook.js

--- Zapier/form_webhook.js
+++ Zapier/form_webhook.js
@@ -1,6 +1,8 @@
+// Replace http://yourwebhookurl.com/ with your url.
+
 jQuery('form').submit(function(e) {
     e.preventDefault();
     jQuery.theForm = jQuery(this);
     var newImage = new Image();
-    newImage.src = "http://yourwebhookurl.com/?" + jQuery.theForm.serialize();
+    newImage.src = "http://yourwebhookurl.com/" + "?" + jQuery.theForm.serialize();
 });
