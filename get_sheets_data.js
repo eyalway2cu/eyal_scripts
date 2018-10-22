fccd897fb9b7315962d9d152a4baf6651610ca19
Eyal Gershon <eyal@way2cu.com>
2018-10-22 15:24:23 +0300

    Create get_sheets_data.js

---
+++ get_sheets_data.js
@@ -0,0 +1,54 @@
+<script>
+//function that parses the google sheet and gets data in XML format
+	function httpGet(theUrl)
+	{
+	    var xmlHttp = new XMLHttpRequest();
+	    xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
+	    xmlHttp.send( null );
+	    parser = new DOMParser();
+	    var xmlDoc = parser.parseFromString(xmlHttp.responseText, "text/xml");
+		return xmlDoc;
+	}
+
+	// Changes XML to JSON
+	function xmlToJson(xml) {
+		
+		// Create the return object
+		var obj = {};
+
+		if (xml.nodeType == 1) { // element
+			// do attributes
+			if (xml.attributes.length > 0) {
+			obj["@attributes"] = {};
+				for (var j = 0; j < xml.attributes.length; j++) {
+					var attribute = xml.attributes.item(j);
+					obj["@attributes"][attribute.nodeName] = attribute.nodeValue;
+				}
+			}
+		} else if (xml.nodeType == 3) { // text
+			obj = xml.nodeValue;
+		}
+
+		// do children
+		if (xml.hasChildNodes()) {
+			for(var i = 0; i < xml.childNodes.length; i++) {
+				var item = xml.childNodes.item(i);
+				var nodeName = item.nodeName;
+				if (typeof(obj[nodeName]) == "undefined") {
+					obj[nodeName] = xmlToJson(item);
+				} else {
+					if (typeof(obj[nodeName].push) == "undefined") {
+						var old = obj[nodeName];
+						obj[nodeName] = [];
+						obj[nodeName].push(old);
+					}
+					obj[nodeName].push(xmlToJson(item));
+				}
+			}
+		}
+		return obj;
+	};
+  
+  // JSON object saved to data variable
+	var data = xmlToJson(httpGet("https://spreadsheets.google.com/feeds/cells/sheetid/od6/public/basic"));
+</script>
