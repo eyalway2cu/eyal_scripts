eda989427ea3d4c2631b1e52a2e6d6208b55de7a
Eyal Gershon <eyal@way2cu.com>
2019-02-17 10:39:43 +0200

    Create getcidfromcookie.js
    Save cid from cookie to cid variable

---
+++ getcidfromcookie.js
@@ -0,0 +1,8 @@
+function getCookie(name) {
+    function escape(s) { return s.replace(/([.*+?\^${}()|\[\]\/\\])/g, '\\$1'); };
+    var match = document.cookie.match(RegExp('(?:^|;\\s*)' + escape(name) + '=([^;]*)'));
+    return match ? match[1] : null;
+}
+
+var cid = getCookie("_ga");
+cid = cid.substring(6);
