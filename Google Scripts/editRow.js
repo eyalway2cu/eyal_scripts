2a95697266115fea383eca793bef2f975ca0e595
Eyal Gershon <eyal@way2cu.com>
2020-01-13 14:03:19 +0200

    Update editRow.js

--- Google Scripts/editRow.js
+++ Google Scripts/editRow.js
@@ -1,8 +1,11 @@
 function editRow(event) {
     var sheet = event.source; //get sheet
+    var sheetname = sheet.getSheetName();//get sheet name
+    if (sheetname != "sheetname")//condition to match certain sheet comment out if not required
+      return;
     var headings = sheet.getDataRange().offset(0, 0, 1).getValues()[0]; //get heading
     var edit_range = event.range; //get edited range
-    var column_to_watch = 2; //A=1, B=2, C=3 etc...
+    var column_to_watch = 3; //A=1, B=2, C=3 etc...
     if (edit_range.getColumn() != column_to_watch)
         return;
     var row = edit_range.getRow();
@@ -10,7 +13,7 @@ function editRow(event) {
         row, // starting row
         1, // starting column
         1, // number of rows
-        5 // number of columns
+        4 // number of columns
     );
     var payload = {};
     for (i = 0; i < headings.length; i++) {
@@ -24,5 +27,5 @@ function editRow(event) {
         'contentType': 'application/json',
         'payload': JSON.stringify(payload)
     };
-    UrlFetchApp.fetch('https://eyaltest.requestcatcher.com/', options);
+    UrlFetchApp.fetch('https://hook.integromat.com/fnrn3e6ia655rhkus5vjp8i4fri9sd28', options);
 }
