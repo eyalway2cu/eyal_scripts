55988d94341eee90abd175c8115763a8879aedd6
Eyal Gershon <eyal@way2cu.com>
2020-01-20 22:42:31 +0200

    Update geturlVariable.js

--- geturlVariable.js
+++ geturlVariable.js
@@ -1,5 +1,4 @@
-function getQueryVariable(variable)
-{
+function getQueryVariable(variable){
        var query = window.location.search.substring(1);
        var vars = query.split("&");
        for (var i=0;i<vars.length;i++) {
