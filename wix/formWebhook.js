7636fd3dcc733a6f7b2f255419a1ca001f5954ed
Eyal Gershon <eyal@way2cu.com>
2020-01-20 15:31:14 +0200

    Create formWebhook.js

---
+++ wix/formWebhook.js
@@ -0,0 +1,26 @@
+import wixCrm from 'wix-crm-backend';
+
+function sendData(item) {
+    fetch("YOURWEBHOOKADDRESS", {
+            "method": "post",
+            "headers": {
+                "Content-Type": "application/json"
+            },
+            "body": JSON.stringify(item)
+        })
+        .then((httpResponse) => {
+            if (httpResponse.ok) {
+                return httpResponse.json();
+            } else {
+                return Promise.reject("Fetch did not succeed");
+            }
+        })
+        .then((json) => console.log(json))
+        .catch(err => console.log(err));
+}
+
+
+export function submissions_afterInsert(item, context) {
+	sendData(item);
+	return item
+}
