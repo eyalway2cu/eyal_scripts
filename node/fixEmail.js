2c7deb29a3426deb266ec2a79ce612db0462b15d
Eyal Gershon <eyal@way2cu.com>
2020-01-22 09:40:06 +0200

    Create fixEmail.js

---
+++ node/fixEmail.js
@@ -0,0 +1,44 @@
+const csv = require('csv-parser');
+const fs = require('fs');
+const http = require('http');
+const mailcheck = require('mailcheck');
+const createCsvWriter = require('csv-writer').createObjectCsvWriter;
+const writeData = [];
+
+const filepath = "data.csv"
+
+fs.createReadStream(filepath)
+    .on('error', () => {
+        // handle error
+    })
+
+    .pipe(csv())
+    .on('data', (row) => {
+        let email = `${row["Email"]}`;
+        mailcheck.run({
+        	email: email,
+        	suggested: function(suggestion) {
+        		//console.log(suggestion.full);
+        		writeData.push({email: email, fixedemail: suggestion.full})
+        	},
+        	empty: function() {
+
+        	}
+        });
+    })
+    .on('end', () => {
+        // handle end of CSV
+        const csvWriter = createCsvWriter({
+          path: 'datafixed.csv',
+          header: [
+            {id: 'email', title: 'Email'},
+            {id: 'fixedemail', title: 'FixedEmail'},
+          ]
+        });
+
+        console.log(writeData);
+        csvWriter
+          .writeRecords(writeData)
+          .then(()=> console.log('The CSV file was written successfully'));
+    })
+
