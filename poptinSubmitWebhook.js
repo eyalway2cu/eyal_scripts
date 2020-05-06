7b9c3e90f059081cbf0557a7048c2729bed0c646
Eyal Gershon <eyal@way2cu.com>
2020-05-06 10:12:27 +0300

    Update poptinSubmitWebhook.js

--- poptinSubmitWebhook.js
+++ poptinSubmitWebhook.js
@@ -1,3 +1,10 @@
+//This code will grab submission data from a Poptin form and submit the data in JSON format to your selected endpoint.
+
+//If you encounter CORS issues, make sure you set the appropriate accept headers in your endpoint such as:
+//Access-Control-Allow-Origin: *
+//Access-Control-Allow-Methods: GET, POST, OPTIONS
+//Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept
+
 var data = {};
 for (i=0;i<poptinSubmitted["YOURPOPUPID"].fields.length;i++){
     data[poptinSubmitted["YOURPOPUPID"].fields[i].name] = poptinSubmitted["YOURPOPUPID"].fields[i].value;
