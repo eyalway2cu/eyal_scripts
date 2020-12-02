6416f86a52104adedca02f1dc80cc15277a5ff93
Eyal Gershon <eyal@way2cu.com>
2020-12-02 09:41:21 +0200

    Update formWebhook.js

--- wix/formWebhook.js
+++ wix/formWebhook.js
@@ -2,6 +2,7 @@ import wixCrm from 'wix-crm-backend';
 import { fetch } from 'wix-fetch';
 
 function sendData(item) {
+//replace YOURWEBHOOKADDRESS with a webhook address (i.e https://somewebsite.com)
     fetch("YOURWEBHOOKADDRESS", {
             "method": "post",
             "headers": {
@@ -20,7 +21,7 @@ function sendData(item) {
         .catch(err => console.log(err));
 }
 
-
+//replace datasetname with your dataset name, if your dataset is called submissions then you change the function name to submissions_afterInsert
 export function datasetname_afterInsert(item, context) {
 	sendData(item);
 	return item
