5058bda280f8eef7fbb791ee61d04ff5d0a46820
Eyal Gershon <eyal@way2cu.com>
2020-12-13 14:38:31 +0200

    Update +972prefix.js

--- Zapier/+972prefix.js
+++ Zapier/+972prefix.js
@@ -4,12 +4,11 @@ if (typeof number !== 'undefined') {
     var number = number.trim();
     var number = number.replace(new RegExp('-', 'g'), '');
     var number = number.replace(/\D/g,'');
-
-    if (number.indexOf("0") == 0) {
-        number = number.replace("0", "+972");
-    }
     if (number.indexOf("00") == 0) {
         number = number.replace("00", "+972");
+    }
+    if (number.indexOf("0") == 0) {
+        number = number.replace("0", "+972");
     }
     output = [{
        number: number
