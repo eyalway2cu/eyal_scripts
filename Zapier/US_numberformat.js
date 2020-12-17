cc47300deaf59d18ee70a13880e43da1b0583de0
Eyal Gershon <eyal@way2cu.com>
2020-12-17 15:55:38 +0200

    Create US_numberformat.js

---
+++ Zapier/US_numberformat.js
@@ -0,0 +1,17 @@
+var number = input.number;
+
+if (typeof number !== 'undefined') {
+    var number = number.trim();
+    var number = number.replace(new RegExp('-', 'g'), '');
+    var number = number.replace(/\D/g,'');
+
+    if (number.indexOf("+") > -1) {
+        number = number.replace("+", "");
+    }
+    if (number.indexOf("1") == 0) {
+        number = number.replace("1", "");
+    }
+    output = [{
+       number: number
+    }];
+}
