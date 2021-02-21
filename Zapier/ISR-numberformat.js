2778aa6fcca3130939ee992f7e1162e11c3aec6c
Eyal Gershon <eyal@way2cu.com>
2021-02-21 10:18:55 +0200

    Update ISR-numberformat.js

--- Zapier/ISR-numberformat.js
+++ Zapier/ISR-numberformat.js
@@ -2,12 +2,8 @@ var number = input.number;
 
 if (typeof number !== 'undefined') {
     var number = number.trim();
-    var number = number.replace(new RegExp('-', 'g'), '');
     var number = number.replace(/\D/g,'');
 
-    if (number.indexOf("+") > -1) {
-        number = number.replace("+", "");
-    }
     if (number.indexOf("972") == 0) {
         number = number.replace("972", "0");
     }
