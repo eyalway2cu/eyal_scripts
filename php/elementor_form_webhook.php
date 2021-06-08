0e1b4f33254ae960444ad450dcda97f2233d4635
Eyal Gershon <eyal@way2cu.com>
2021-06-08 17:39:57 +0300

    Create elementor_form_webhook.php

---
+++ php/elementor_form_webhook.php
@@ -0,0 +1,32 @@
+<?php
+/* 
+This code will send a webhook to your desired URL after a succesfull Elementor form submission.
+It will not interfere with any webhooks that are set on the form.
+*/
+function sendSecondWebhookElementor( $record, $ajax_handler ){
+    // get form settings
+    $form_settings = $record->get('form_settings');
+    // get form ID
+    $form_id = $form_settings['form_id'];
+
+    // get fields using method in Form_Record class
+    $raw_fields = $record->get('fields');
+        $fields = [];
+    foreach ( $raw_fields as $id => $field ) {
+            $fields[$id] = $field['value'];
+    }
+
+    $url = "WEBHOOKURL";
+    $opts=array("http"=>array(
+        "method"=>"POST",
+        "header"=>"Content-type: application/json",
+        "content"=>json_encode($fields)
+        ));
+    $context = stream_context_create($opts);
+    $result = file_get_contents($url, false, $context);
+    $response = json_decode($result);
+    return $response;
+}
+
+add_action( 'elementor_pro/forms/validation', 'sendSecondWebhookElementor', 10, 2 );
+?>
