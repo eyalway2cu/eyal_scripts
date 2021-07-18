49234b49b421b138e7ce668c78a359e09e0c8c5b
Eyal Gershon <eyal@way2cu.com>
2021-07-18 10:48:16 +0300

    Update generic_sid_injector.js

--- callbox/generic_sid_injector.js
+++ callbox/generic_sid_injector.js
@@ -2,6 +2,7 @@ var swapStatus = false;
 window.CTMSourceEvent = function(event) {
     if (event.state == "after"){
         if (!swapStatus){
+            var forms = document.querySelectorAll("form");
             for (i=0;i<forms.length;i++){
                 if (forms[i].querySelector("input[name='sid']")){
                     forms[i].querySelector("input[name='sid']").setAttribute("value", __ctm.config.sid);
